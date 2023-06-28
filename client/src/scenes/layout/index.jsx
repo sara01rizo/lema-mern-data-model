5fe133173ea1e6507f92ec3264e564304e4f1afe
sara pulido <sara01rizo@gmail.com>
2023-06-29 01:26:40 +0200

    Create: siderbar component use Drawer from MUI 🥨

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
-import { Box, useMediaQuery } from "@mui/material";
+import { Box, useMediaQuery } from '@mui/material';
 import { Outlet } from 'react-router';
-import { useSelect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Navbar from '../../components/Navbar';
+import Sidebar from 'components/Sidebar';
 
 const Layout = () => {
-  return <Box width="100%" height="100%">
-    <Box>
-      <Navbar />
-      <Outlet />
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  return (
+    <Box diaplay={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
     </Box>
-  </Box>;
+  );
 };
 
 export default Layout;

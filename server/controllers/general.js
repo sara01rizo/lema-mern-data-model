6c338c62bf83f9e8ece9e4ceb7ed865e9d021d3b
sara pulido <sara01rizo@gmail.com>
2023-06-29 13:26:29 +0200

    Express and Mongoose to get UserData

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -6,8 +6,9 @@ export const getUser = async (req, res) => {
     const user = await User.findById(id);
     res.status(200).json(user);
   } catch (error) {
-    res.status(404).json({ message: error.message })
+    res.status(404).json({ message: error.message });
   }
-}
+};
 
 // Pass the :id to grab the user
+//
--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { getUSer } from "../controllers/general.js";
 
 const router = express.Router();
 
+router.get("/user/:id", getUSer);
+
 export default router;
+
+// Pass the :id to grab the user

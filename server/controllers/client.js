1399a3619d02d66b4ae031d35b66cb282b69d7a0
sara pulido <sara01rizo@gmail.com>
2023-07-01 00:09:15 +0200

    Build: products model relevant to each product

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,1 +1,24 @@
-//controllers
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    const ProductWithStatus = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+
+    res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};

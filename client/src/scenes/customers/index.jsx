bc3b6f2333138ae8c596b7cc4457f5361fdc2498
sara pulido <sara01rizo@gmail.com>
2023-07-06 09:13:03 +0200

    Create: daily scene component

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -7,7 +7,6 @@ import { useGetCustomersQuery } from 'state/api';
 const Customers = () => {
   const theme = useTheme();
   const { data, isLoading } = useGetCustomersQuery();
-  console.log("🥨  Customers  data", data);
 
   const columns = [
     {

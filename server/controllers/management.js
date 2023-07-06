97d9dc895a5a9c6344df3e3121d4c41b64592b4e
sara pulido <sara01rizo@gmail.com>
2023-07-06 12:00:56 +0200

    Task: complete api for admins

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,1 +1,12 @@
-//controllers
+import mongoose from "mongoose";
+import User from "../models.User.js";
+
+export const getAdmins = async (req, res) => {
+  try {
+    const admins = await User.find({ role: "admin" }).select("-password");
+    res.status(200).json(admins);
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+
+};

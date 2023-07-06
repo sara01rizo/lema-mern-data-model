09177e6e1ecc57980c73c02d759230d873d909ff
sara pulido <sara01rizo@gmail.com>
2023-07-06 13:30:54 +0200

    Fixed: nodemon error

--- server/models/User.js
+++ server/models/User.js
@@ -27,7 +27,7 @@ const UserSchema = new mongoose.Schema(
     transactions: Array,
     role: {
       type: String,
-      enum: ["user", "admin", "superadmin"],
+      enum: ["user", "admin", "superadmin"], //role
       default: "admin",
     },
   },
